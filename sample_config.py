76b3342d1256bd3806696084bb22eaea0e531aca
Matthew Lewis <matt@mplewis.com>
2014-03-11 02:34:39 -0500

    Initial commit

---
+++ sample_config.py
@@ -0,0 +1,2 @@
+class MongoDbConfig:
+    uri = 'mongodb://my-mongo-server.com/mydb'  # noqa
---
+++ server.py
@@ -0,0 +1,5 @@
+import config
+from pokemon_save_parser import save_parser
+
+print save_parser
+print config.MongoDbConfig.uri
